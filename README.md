fd068d95d1c45d3bd0dd085b6e0129e209debd47
Mizack <mizack1234@gmail.com>
2022-05-01 09:44:44 -0300

    primeiros passos com express

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # inicio_node
 primeiros passos com node
+
+# para instalar o express
+
+`npm install express --save`
+
---
+++ aula_4/index.js
@@ -0,0 +1,22 @@
+var express = require("express")
+// instanciando o framework
+const app = express()
+
+app.get('/',function(req,res){
+    res.send('bem vido rapa')
+})
+app.get('/sobre',function(req,res){
+    // res.send('testando rotas')
+    res.sendFile("C:/Users/mizac/Documents/projeto_node/aula_4/index.html")
+})
+// rotas dinamicas com parametros
+// sends são enviados uma unica vez
+app.get('/ola/:cargo/:nome',function(req,res){
+    res.send(req.params.nome+req.params.cargo)
+})
+
+// iniciando servidor e indicando a porta
+// nada que compoem a aplicação pode ficar abaixo do app.listem()
+app.listen(8081,function(){
+    console.log('servidor rodando')
+})
